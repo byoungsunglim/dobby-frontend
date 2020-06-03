32b1a3121a6c8ba611bc0a3cd1f5e8b9f56b624d
ryanlim95 <ryanlim1995@gmail.com>
2020-06-03 22:05:21 +0900

    canvas resizing math revised

--- src/components/Design.js
+++ src/components/Design.js
@@ -38,10 +38,10 @@ class Design extends Component {
   }
 
   handleResize = () => {
-    const design_width = window.innerWidth - 200 - 5 -parseInt(document.getElementById("draft").style.width) - 20;
+    const design_width = window.innerWidth - 200 -parseInt(document.getElementById("draft").style.width) - 20;
     document.getElementById("design").style.width = `${design_width}px`;
     document.querySelectorAll("[class=canvas]").forEach((node, idx) => {
-      node.style.transform = `scale(${(design_width - 40 * (design_width)/1920) / 1920}) translate(0, ${idx * (-1080 * 1920 / (design_width - 40 * (design_width)/1920) + 1080 + 20)}px)`;
+      node.style.transform = `scale(${(design_width - 6 * (design_width)/1920) / 1920}) translate(0, ${idx * (-1080 * 1920 / (design_width - 6 * (design_width)/1920) + 1080 + 20)}px)`;
     });
 
     if (window.innerWidth < 400) {
