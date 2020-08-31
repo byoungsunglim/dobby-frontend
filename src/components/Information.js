ebfd0e2910de6c261688ba4f70dbd2b1fb7d7096
ryanlim95 <ryanlim1995@gmail.com>
2020-08-31 21:24:19 +0900

    Information created

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
-import "../assets/css/Information.css";
+import "../assets/css/Information.scss";
 
 class Information extends Component {
-  state = {};
-
   render() {
     return (
-        <div id="information">
+      <div id="information">
+        <div id="information_header">
+          <img alt="profile" src={this.props.user.profile_image} />
+          <span>{this.props.user.nickname}</span>
         </div>
-    )
+        <div id="information_body">
+          <div className="info_section" id="todos">
+            <span className="info_section_title">할 일</span>
+            <hr />
+          </div>
+          <hr />
+          <div className="info_section" id="histories">
+            <span className="info_section_title">기록</span>
+            <hr />
+          </div>
+          <hr />
+          <div className="info_section" id="selected_files">
+            <span className="info_section_title">선택 파일</span>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
